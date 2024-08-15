9a92ffb40571a98673070b11d561c540918da939
ravizhan <ravizhan@hotmail.com>
2024-08-16 00:56:34 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# screenshot_classify
-赛博拍屏鉴定师，帮您一眼丁真分辨截图与拍屏
+# 赛博拍屏鉴定师——帮您一眼丁真分辨截图与拍屏
+## 说明
+
+本项目模型基于 `yolov8s-cls` 模型(v8.20)训练，未来视情况将采用更大更新的模型。
+
+由于训练集不足，当前准确率仍然抽象。
+
+模型会将图片分为三类
+
+- cameracap: 拍屏
+
+- others: 啥也不是
+- screenshot: 截图
+
+## 使用
+
+- 将测试图片放入 `test_img` 目录下
+
+- 安装依赖
+
+  ```bash
+  pip install -r requirements.txt
+  ```
+
+- 运行 `main.py`
+
+  ``` bash
+  python ./main.py
+  ```
+
+## 贡献
+
+如果您觉得模型效果不够好，不要犹豫，将您的抽象图片即刻通过 [Pull Request](https://github.com/ravizhan/screenshot_classify/pulls) 提交。我将尽快更新模型。
+
+请将图片归类放入对应的文件夹内，文件名统一为 `MD5hash` ，以避免重名，文件格式接受 `JPG` 和 `PNG`。
+
+## 鸣谢
+
+感谢 [ultralytics](https://github.com/ultralytics/ultralytics) 提供yolov8预训练模型
+
+感谢[所有贡献者](https://github.com/ravizhan/screenshot_classify/graphs/contributors)，无论您是提供了图片或是代码
+
---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+import onnxruntime
+import numpy as np
+import cv2
+import os
+
+tag = {0: 'cameracap', 1: 'others', 2: 'screenshot'}
+
+session = onnxruntime.InferenceSession("best-dynamic.onnx")
+model_inputs = session.get_inputs()
+
+filenames = os.listdir(r'./test_img')
+for file in filenames:
+    img = cv2.cvtColor(cv2.imread("./test_img/"+file), cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (1024, 1024))
+    # cv2.imwrite("test.png", img)
+    image_data = np.array(img) / 255.0
+    image_data = np.transpose(image_data, (2, 0, 1))
+    image_data = np.expand_dims(image_data, axis=0).astype(np.float32)
+    input = {"images": image_data}
+    t = time.time()
+    output = session.run(None, input)
+    tt = (time.time() - t)*1000
+    predicted_class = np.argmax(output[0], axis=1)[0]
+    confidence = np.max(output[0], axis=1)[0]
+    print(file,tag[predicted_class], confidence * 100,tt,"ms")
