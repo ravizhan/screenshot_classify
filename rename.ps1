870596144d835b9cce136d74a96aee558ef79e38
Syphlix Oauthes <46254933+llw1573@users.noreply.github.com>
2024-08-17 20:56:01 +0800

    Revert "Change Hash Algorithm"
    This reverts commit 56238bef772a4a7233a98377811f7c41799f6ea1.

--- rename.ps1
+++ rename.ps1
@@ -2,9 +2,9 @@ $directoryPath = "./img"
 $files = Get-ChildItem -Path $directoryPath -Recurse -File
 
 foreach ($file in $files) {
-    $sha256 = Get-FileHash -Path $file.FullName -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+    $md5 = Get-FileHash -Path $file.FullName -Algorithm MD5 | Select-Object -ExpandProperty Hash
     $extension = $file.Extension
-    $newFileName = "$sha256$extension"
+    $newFileName = "$md5$extension"
     $newFilePath = Join-Path -Path $file.DirectoryName -ChildPath $newFileName
     Rename-Item -Path $file.FullName -NewName $newFilePath
     Write-Host "Renamed '$($file.FullName)' to '$newFilePath'"
