c1b17ac5bddcf837552c1cf5752ad42de88ddc9c
Syphlix Oauthes <46254933+llw1573@users.noreply.github.com>
2024-08-21 00:16:15 +0800

    Upload some Picture and rename string in rename.ps1

--- rename.ps1
+++ rename.ps1
@@ -2,9 +2,9 @@ $directoryPath = "./img"
 $files = Get-ChildItem -Path $directoryPath -Recurse -File
 
 foreach ($file in $files) {
-    $md5 = Get-FileHash -Path $file.FullName -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+    $hash = Get-FileHash -Path $file.FullName -Algorithm SHA256 | Select-Object -ExpandProperty Hash
     $extension = $file.Extension
-    $newFileName = "$md5$extension"
+    $newFileName = "$hash$extension"
     $newFilePath = Join-Path -Path $file.DirectoryName -ChildPath $newFileName
     Rename-Item -Path $file.FullName -NewName $newFilePath
     Write-Host "Renamed '$($file.FullName)' to '$newFilePath'"
